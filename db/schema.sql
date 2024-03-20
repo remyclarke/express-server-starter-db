d596ef625de3c1801abc1ec7d711ab5738e99ddb
jd richards <jdrichardstech@gmail.com>
2024-03-20 09:26:05 -0400

    add instr and code

---
+++ db/schema.sql
@@ -0,0 +1,6 @@
+DROP DATABASE IF EXISTS myDB -- change name of db
+CREATE DATABASE myDB -- change name of db
+
+\c myDB -- change name of db
+
+-- Create your tables here
---
+++ db/seed.sql
@@ -0,0 +1,4 @@
+\c myDB -- change name of db
+
+
+-- Create your seedfiles here using SQL -  INSERT INTO() VALUES()
